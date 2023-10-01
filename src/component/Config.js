7d365c92bc196a04e692daca0d522f4f24aa1c93
LEGION <koobear99@gamil.com>
2023-10-01 09:35:07 +0545

    update url

--- src/component/Config.js
+++ src/component/Config.js
@@ -1,3 +1,3 @@
-const API_URL = 'http://localhost:8080/api';
-
+// const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://goku-47elh85zg-12-twelvve.vercel.app/';
 export default API_URL;
